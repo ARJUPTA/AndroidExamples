e970a5755d3ea300b00a8c4c2bec0f59374168dd
ARJUPTA <64064110+ARJUPTA@users.noreply.github.com>
2020-09-30 23:51:42 +0530

    MVP of Drawer layout completed from blog of Journal Dev

---
+++ Drawer/app/src/main/java/com/arjupta/drawer/ConnectFragment.java
@@ -0,0 +1,24 @@
+package com.arjupta.drawer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ConnectFragment extends Fragment {
+
+    public ConnectFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_connect, container, false);
+
+        return rootView;
+    }
+
+}
