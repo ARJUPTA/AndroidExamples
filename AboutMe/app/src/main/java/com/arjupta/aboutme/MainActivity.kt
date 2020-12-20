c7286bff8732198301842dc701304f10b0bf50bc
ARJUPTA <64064110+ARJUPTA@users.noreply.github.com>
2020-12-20 16:31:56 +0530

    Completed Aboute Me + view binding

--- AboutMe/app/src/main/java/com/arjupta/aboutme/MainActivity.kt
+++ AboutMe/app/src/main/java/com/arjupta/aboutme/MainActivity.kt
@@ -5,29 +5,30 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.arjupta.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val data : MyName = MyName("Arjun Gupta","")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.done).setOnClickListener{
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.myName = data
+        binding.done.setOnClickListener{
             addNickname(it)
         }
     }
 
     private fun addNickname(view : View){
-        val nicknameEditText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
-
-        nicknameTextView.text = nicknameEditText.text
-
-        nicknameEditText.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
-        view.visibility = View.GONE
-
+        binding.apply {
+            myName?.nickName = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+            view.visibility = View.GONE
+        }
         val keyboard = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         keyboard.hideSoftInputFromWindow(view.windowToken,0)
     }
